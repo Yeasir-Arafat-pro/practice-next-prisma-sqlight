55c55ce5b0a96342874cb3dedf354823069719f0
Yeasir Arafat <yeasirarafat.pro@gmail.com>
2025-08-31 15:05:29 +0600

    practice next with prisma

--- src/app/(users)/users/page.tsx
+++ src/app/(users)/users/page.tsx
@@ -2,7 +2,7 @@ import { User } from '@/types/userType'
 import React from 'react'
 
 export default async function page() {
-  const res = await fetch('http://localhost:3000/api/users')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ""}/api/users`)
   const data = await res.json()
   console.log(data);
 
