68e0882770a035250d1f39b18e5cd47fd98f18b1
Yeasir Arafat <yeasirarafat.pro@gmail.com>
2025-08-31 15:10:10 +0600

    practice next with prisma

--- src/app/(users)/users/page.tsx
+++ src/app/(users)/users/page.tsx
@@ -1,14 +1,13 @@
+import { _GetData } from '@/app/prisma-db'
 import { User } from '@/types/userType'
 import React from 'react'
 
 export default async function page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ""}/api/users`)
-  const data = await res.json()
-  console.log(data);
+  const res = await _GetData('user')
 
   return (
     <div className='flex flex-col gap-4 p-4'>
-      {data.payload.map((user: User) => (
+      {res.map((user: User) => (
         <div className='border p-4 rounded' key={user.email}>
           <h2 className='text-xl font-bold'>{user.name}</h2>
           <p className='text-gray-600'>{user.email}</p>
